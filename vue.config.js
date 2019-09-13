2bd4d1cd3f1ba58ed0b8860dd9b5d32581a8d6e0
RoelZ <roel.heesterbeek@gmail.com>
2019-09-13 13:29:40 +0200

    firebase init and pwa manifest

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ module.exports = {
         }
     }
   },
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/apps/admin/'
-    : '/',
+  // publicPath: process.env.NODE_ENV === 'production'
+  //   ? '/apps/admin/'
+  //   : '/',
 }
