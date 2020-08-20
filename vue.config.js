8fed4a54f4fc9ed303a17d45fddcee5bcea8322b
Roel Heesterbeek <roel.heesterbeek@gmail.com>
2020-08-20 16:13:30 +0200

    adds proxies for APIs

--- vue.config.js
+++ vue.config.js
@@ -4,10 +4,18 @@ module.exports = {
       '/wp-json/': {
         target: 'https://www.placethemoment.com/',
         changeOrigin: true
+      },
+      '/pie/psdService/': {
+        target: 'https://image.adobe.io/',
+        changeOrigin: true
+      },
+      '/api/v2/': {
+        target: 'https://panel.sendcloud.sc/',
+        changeOrigin: true
       }
     }
   },
   publicPath: process.env.NODE_ENV === 'production'
-    ? '/kcfuy/'
+    ? '/apps/dashboard/'
     : '/',
 }
