7a433f38239c8dee42d848c491931f96584f3ef1
Roel Heesterbeek <roel.heesterbeek@gmail.com>
2020-09-16 10:22:55 +0200

    updates dependencies and configs

--- vue.config.js
+++ vue.config.js
@@ -1,21 +1,14 @@
 module.exports = {
   devServer: {
     proxy: {
-      '/wp-json/': {
-        target: 'https://www.placethemoment.com/',
-        changeOrigin: true
-      },
       '/pie/psdService/': {
         target: 'https://image.adobe.io/',
         changeOrigin: true
       },
-      '/api/v2/': {
-        target: 'https://panel.sendcloud.sc/',
-        changeOrigin: true
-      }
+      // '/api/v2/': {
+      //   target: 'https://panel.sendcloud.sc/',
+      //   changeOrigin: true
+      // }
     }
   },
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/apps/dashboard/'
-    : '/',
 }
